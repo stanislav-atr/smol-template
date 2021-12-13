5f00882dbdc6ca47ed69253c8c1007b264aa81d7
caffeine <s.atroschenko@adguard.com>
2021-12-13 23:03:44 +0300

    dont bundle comments

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,23 @@
-//import path from 'path/posix';
-//import banner from 'rollup-plugin-banner';
 import clear from 'rollup-plugin-clear';
+import cleanup from 'rollup-plugin-cleanup';
 
 export default {
     input: './index.js',
     output: {
-        file: './dist/bundle.js',
+        file: './dist/badoo-auto-filter.userscript.js',
         format: 'cjs'
     },
     plugins: [
-        // banner({
-        //     file: path.join(__dirname, './prefixTamperMonkey.js'),
-        //     encoding: 'utf-8', // default is utf-8
-        // }),
+        // Clean dist directory before bundling
         clear({
-            // required, point out which directories should be clear.
+            // required, point out which directories should be cleared.
             targets: ['dist'],
             // optional, whether clear the directores when rollup recompile on --watch mode.
-            watch: true, // default: false
+            watch: true,
+        }),
+        // Don't include comments in bundle
+        cleanup({
+            comments: 'eslint',
         }),
     ],
 };
